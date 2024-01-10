7846b61bea89c5d69cfc117c052f38e202f49d75
Соколовский Иван Павлович <ivan3177@gmail.com>
2024-01-10 11:52:14 +0400

    feat(app): add reanimated and gesture handler dependencies

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {Navigator} from './navigator.tsx';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 export const App = () => {
-  return <Navigator />;
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Navigator />
+    </GestureHandlerRootView>
+  );
 };
