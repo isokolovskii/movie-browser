c0f628f26e104bbcc7facbe0be3768d16375850c
Соколовский Иван Павлович <ivan3177@gmail.com>
2024-01-10 14:06:54 +0400

    feat(assets): add tool for manipulating native assets

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+/** @type import("@react-native-community/cli-types").Config */
+module.exports = {
+  assets: [],
+  iosAssets: [],
+  androidAssets: [],
+  project: {
+    ios: {
+      automaticPodsInstallation: true,
+    },
+  },
+};
